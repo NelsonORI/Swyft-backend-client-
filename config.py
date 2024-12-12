3e0ab193a33e7a96c06333cceb762578b1ed3c78
Nelson Orina <orinamecha2@gmail.com>
2024-12-12 19:45:21 +0300

    Update to CORS policy

--- config.py
+++ config.py
@@ -4,13 +4,15 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 from flask_bcrypt import Bcrypt 
-# from flask_cors import CORS 
+from flask_cors import CORS 
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SWYFT_USER')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
+CORS(app, resources={r"/*":{"origins":"*"}})
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
